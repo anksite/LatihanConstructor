d5d9ded8ddfd3aea97fc18a5285c47b431ffb97d
anksite <adnankurniawan98@gmail.com>
2019-04-02 15:03:35 +0700

    commit awal

---
+++ LatihanConstruktor/Mahasiswa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanConstruktor {
+    class Mahasiswa {
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+
+        public Mahasiswa() { }
+
+        public Mahasiswa(string nim, string nama) {
+            NIM = nim;
+            Nama = nama;
+        }
+    }
+}
---
+++ LatihanConstruktor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanConstruktor {
+    class Program {
+        static void Main(string[] args) {
+            Mahasiswa mhs1 = new Mahasiswa();
+            mhs1.NIM = "5577";
+            mhs1.Nama = "Doni";
+            cetakHasil(mhs1);
+
+            Console.WriteLine();
+
+            Mahasiswa mhs2 = new Mahasiswa("1234", "Paijo");
+            cetakHasil(mhs2);
+
+            Console.ReadKey();
+        }
+
+        static void cetakHasil(Mahasiswa mhs) {
+            Console.WriteLine("NIM: {0}", mhs.NIM);
+            Console.WriteLine("Nama: {0}", mhs.Nama);
+        }
+    }
+}
